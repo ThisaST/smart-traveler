b7188d1d1dc074ea887ab193288a54367a47d1cf
ThisaST <thisa030@gmail.com>
2018-03-01 02:20:33 +0530

    change the package of SignInActivity

---
+++ app/src/main/java/com/uok/se/thisara/smart/smarttraveler/App.java
@@ -0,0 +1,12 @@
+package com.uok.se.thisara.smart.smarttraveler;
+
+import android.app.Application;
+
+/**
+ * Created by malware on 3/1/18.
+ */
+
+public class App extends Application {
+
+
+}
--- app/src/main/java/com/uok/se/thisara/smart/smarttraveler/authentication/SignInActivity.java
+++ app/src/main/java/com/uok/se/thisara/smart/smarttraveler/authentication/SignInActivity.java
@@ -1,10 +1,12 @@
-package com.uok.se.thisara.smart.smarttraveler;
+package com.uok.se.thisara.smart.smarttraveler.authentication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+
+import com.uok.se.thisara.smart.smarttraveler.R;
 
 public class SignInActivity extends AppCompatActivity {
 
